597ee9ef59c12c7848038b305ea33f7cc9ed02de
Ashley How <ahow001@e.ntu.edu.sg>
2021-04-17 17:57:34 +0800

    example of get data from api

--- src/login.js
+++ src/login.js
@@ -1,7 +1,74 @@
+import React, { useState, useEffect } from 'react';
+
 function Login() {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [data, setData] = useState([])
+  const [showData, setShowData] = useState(false)
+
+  const getData = async () => {
+    const result = await fetch(
+      'https://api.github.com/users/hacktivist123/received_events'
+    )
+    const res = await result.json()
+    console.log(res)
+    setData(res)
+    setShowData(true)
+  }
+
+  const clearData = () => {
+    setData([])
+  }
+
+  const submit = () => {
+    console.log(data)
+  }
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
   return (
-    <div >
-     LOGIN PAGE
+    <div>
+      <h1>LOGIN PAGE</h1>
+      <label>
+        Username:
+          <input type="text" value={username}
+          onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+          <input type="password"
+          value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button onClick={() => submit()}>
+        Submit
+        </button>
+
+      <div>
+        TEST:
+        <br />
+        <button onClick={() => getData()}>
+          Get data
+        </button>
+        <br />
+        <button onClick={() => clearData()}>
+          Clear data
+        </button>
+        <br />
+        {
+          showData &&
+          data.map((item, index) => {
+            return <ul key={index}>{item.repo.name}</ul>
+          })
+        }
+      </div>
     </div>
   );
 }
