69ab09fd3dde87442ea6e4b15c6177249c5e2b88
Ashley How <ahow001@e.ntu.edu.sg>
2021-04-17 15:11:29 +0800

    change

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+
+function Home() {
+  return (
+    <div>
+      HOME
+    </div>
+  );
+}
+
+export default Home;
